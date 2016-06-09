80b98d19eb8cd9dc90a32c622dec0eae000cf3f5
wangchz13 <wangchz13@gmail.com>
2016-06-09 20:50:39 +0800

    find_jmp_esp找到跳板地址

--- buffer_overflow/cmd.cpp
+++ buffer_overflow/cmd.cpp
@@ -1,8 +1,24 @@
 #include <windows.h>
 
+#include <stdio.h>
+int findJmp(const char* dll_name);
 int main()
 {
 	//LoadLibrary("msvcrt.dll");
-	system("notepad");
+	printf("%d", findJmp("msvcrt.dll"));
+	return 0;
+}
+
+//查询dll内第一个jmp esp指令的位置
+int findJmp(const char* dll_name)
+{
+    char* handle=(char*)LoadLibraryA(dll_name);//获取dll加载地址
+    for(int pos=0;;pos++)//遍历dll代码空间
+    {
+        if(handle[pos]=='M'&&handle[pos+1]=='Z')//寻找0xffe4 = jmp  esp
+        {
+            return (int)(handle+pos);
+        }
+    }
 	return 0;
 }
---
+++ find_jmp_esp.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+
+//查询dll内第一个jmp esp指令的位置
+int findJmp(const char* dll_name)
+{
+    char* handle=(char*)LoadLibraryA(dll_name);//获取dll加载地址
+    for(int pos=0;;pos++)//遍历dll代码空间
+    {
+        if(handle[pos]==0xff&&handle[pos+1]==0xe4)//寻找0xffe4 = jmp  esp
+        {
+            return (int)(handle+pos);
+        }
+    }
+	return 0;
+}
